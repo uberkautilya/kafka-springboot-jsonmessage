377d8457dbde433bc8745452d820c396d49fe58e
Mithun <mithuncm@gmail.com>
2023-01-12 17:39:18 +0530

    Added properties in application.properties for Kafka consumer and producer

---
+++ src/main/java/uber/kautilya/JavaguidesKafkaApplication.java
@@ -0,0 +1,13 @@
+package uber.kautilya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaguidesKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguidesKafkaApplication.class, args);
+	}
+
+}
